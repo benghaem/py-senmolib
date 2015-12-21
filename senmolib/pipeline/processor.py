0bffaf3f93bd7b3973f9adc80fa87cdd41a43e85
Ben Ghaem <ben.ghaem@gmail.com>
2015-12-21 17:49:58 -0500

    documented methods

--- senmolib/pipeline/processor.py
+++ senmolib/pipeline/processor.py
@@ -3,7 +3,18 @@ import zmq as zmq
 import time
 
 class Processor(object):
-	"""Processor launches processes and configures data flow between processing and fusion components"""
+	"""Processor launches real processes and configures data flow between processing and fusion components.
+
+	Processor is useful when one would like to use a mix of senmo components that may be implemented across languages. If one is using components that are only written in Python, PureProcessor may be preferable as all components may be defined within the same file. 
+
+	Args:
+		port_list: A length 4 list of port values. For example, [5000,5001,5002,5003]. The first value is the input port, the second the worker port, the third the fusion port, and the fourth the output port. 
+		process_count: An int. The number of worker processes to start
+		vent_path:   A string. an absolute path to the vent component
+		worker_path: A string. an absolute path to the worker component
+		fusion_path: A string. an absolute path to the fusion component
+		identifier: A string to identify the processor
+	"""
 	def __init__(self, port_list, process_count, vent_path, worker_path, fusion_path, identifier):
 		super(Processor, self).__init__()
 
@@ -27,7 +38,11 @@ class Processor(object):
 		#ZMQ context and socket
 
 	def start(self):
-		"""Start processes"""
+		"""Start processes
+		
+		Starts the defined number of worker processes as well as the fusion and vent components
+
+		"""
 		# start workers
 		self.add_worker_process(self.process_count)
 
@@ -40,6 +55,13 @@ class Processor(object):
 		self.running = True
 
 	def add_worker_process(self, num):
+		"""Starts a additional set of worker processes
+
+		Args: 
+			num: an int. The number of additional  worker processes to start
+
+		"""
+
 		for i in range(num):
 				new_process = Process(self.identifier+"-p-"+str(i+self.iid_offset), self.process_path, self.port_list)
 				self.process_controlers.append(new_process)
@@ -48,7 +70,7 @@ class Processor(object):
 
 
 	def stop(self):
-		"""Stop processes and fusion"""
+		"""Stop all worker processes as well as the fusion and vent components"""
 		for pc in self.process_controlers:
 			pc.stop()
 		self.fusion_controller.stop()
@@ -58,7 +80,12 @@ class Processor(object):
 		self._reset_controllers()
 
 	def reload_processes(self, kill=False, rolling=False):
-		"""Reloads all processes. By default will disable the processor and restart all processes. Enabling rolling will keep the processor running while reloading processes sequentially."""
+		"""Reloads all worker processes. By default will stop and then restart all processes. Enabling rolling will keep the workers running while reloading worker processes sequentially.
+
+		Args:
+			kill: A boolean. If True, kill proesses instead of stopping them
+			rolling: A boolean. If True, reload processes sequentially
+		"""
 		if rolling:
 			for pc in self.process_controlers:
 				pc.restart(kill=kill)
@@ -87,7 +114,7 @@ class Processor(object):
 		self.running = True
 
 	def kill(self):
-		"""Kill processes and fusion"""
+		"""Kill all worker processes as well as fusion and vent components"""
 		for pc in self.process_controlers:
 			pc.kill()
 		self.fusion_controller.kill()
@@ -95,6 +122,7 @@ class Processor(object):
 		self._reset_controllers()
 
 	def restart(self):
+		"""Stop and then restart all worker processes and fusion/vent components"""
 		self.stop()
 		self.start()
 
@@ -110,15 +138,19 @@ class Processor(object):
 
 	def list_pids(self):
 		"""
-		Returns a list of process pids	
+		Returns a list of process pids
+
+		Returns:
+			list: A list of all pids, where the fusion component is first, the vent component is second, and the worker processes follow
 		"""
 		return [self.fusion_controller.get_pid()] + [self.vent_controller.get_pid()] + [pc.get_pid() for pc in self.process_controlers]
 
 	def process_count(self):
 		"""
-		Returns number of active processes
+		Returns number of active worker processes
 
-		*Ignores fusion controller
+		Returns:
+			int: number of active worker processes
 		"""
 
 		return len(self.process_controlers)
