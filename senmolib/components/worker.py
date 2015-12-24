fd97d65c39d84b123755a5f436f8feca7a8f851f
Ben Ghaem <ben.ghaem@gmail.com>
2015-12-24 15:58:29 -0500

    document worker

--- senmolib/components/worker.py
+++ senmolib/components/worker.py
@@ -2,6 +2,12 @@ from .base import Base
 import zmq
 
 class Worker(Base):
+	"""
+	A class that may be subclassed to implement senmo worker components. 
+
+	Note:
+		process() should be redefined by all subclasses as the reference implementation does nothing.
+	"""
 	def __init__(self, **kwargs):
 		super(Worker, self).__init__(**kwargs)
 		# Socket to receive messages on
@@ -13,11 +19,26 @@ class Worker(Base):
 		self.sender.connect("tcp://localhost:"+str(self.fusion_port))
 
 	def start(self):
+		"""
+		Start the worker
+
+		Receives objects from the ventilator and then passes them to the process method
+		"""
 		while self.running:
 			data = self.decode(self.receiver.recv())
 			output = self.process(data)
 			self.sender.send(self.encode(output))
 
 	def process(self, data):
+		"""
+		Process the data object
+
+		Args:
+			data: a python object
+
+		Returns:
+			object: a python object to be sent to fusion
+		
+		"""
 		return data
 
